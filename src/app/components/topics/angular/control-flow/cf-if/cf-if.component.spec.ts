3eae5b37e814c192955891a337011bb91762ff3c
tomisko <toom134@gmail.com>
2024-04-17 23:04:29 +0200

    topic: angular - cf if

---
+++ src/app/components/topics/angular/control-flow/cf-if/cf-if.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CfIfComponent } from './cf-if.component';
+
+describe('CfIfComponent', () => {
+  let component: CfIfComponent;
+  let fixture: ComponentFixture<CfIfComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CfIfComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(CfIfComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/topics/angular/control-flow/cf-if/cf-if.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cf-if',
+  templateUrl: './cf-if.component.html',
+  styleUrl: './cf-if.component.css'
+})
+export class CfIfComponent {
+
+}
