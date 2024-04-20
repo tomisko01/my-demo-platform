471103884f642b2bb005d35ea78094f1d8dbd355
tomisko <toom134@gmail.com>
2024-04-20 22:10:34 +0200

    topic: angular - conditional states - more examples

--- src/app/components/topics/angular/conditional-states/conditional-states.component.ts
+++ src/app/components/topics/angular/conditional-states/conditional-states.component.ts
@@ -16,6 +16,56 @@ export class ConditionalStatesComponent implements AfterViewChecked{
     type: 'BEGINNER'
   }
 
+  ngClassSimpleCode: string =`<div [ngClass]="'beginner'">
+    Sample text with style
+</div>`;
+  ngClassListCode: string =`<div [ngClass]="'beginner border-1px'">
+  Sample text with list of styles
+</div>`;
+  ngClassList2Code: string =`<div [ngClass]="['beginner','border-1px']">
+  Sample text with list of styles
+</div>`;
+  ngClassObjectCode: string = `<div [ngClass]="{'beginner': true, 'border-1px': true}">
+  Sample text with configuration object of styles
+</div>`;
+  ngClassMethodCode: string = `
+//HTML
+<div [ngClass]="divClasses()">
+  Sample text with configuration object of styles from method
+</div>
+
+//COMPONENT
+divClasses() {
+    return {'beginner': true, 'border-1px': true};
+}`;
+  ngClassConditionMethodCode: string = `
+//HTML
+<div [ngClass]="conditionalDivClasses()">
+  {{objectToShow.desc}}
+</div>
+
+//COMPONENT
+conditionalDivClasses() {
+    return {'beginner': this.objectToShow.type === 'BEGINNER'}
+}`;
+  stylesCode: string = `
+<div [style.text-decoration]="'underline'">
+  {{objectToShow.desc}}
+</div>`;
+  ngStylesCode: string =`<div [ngStyle]="{'text-decoration': 'underline'}">
+  {{objectToShow.desc}}
+</div>`;
+  ngStylesMethodCode: string = `
+//HTML
+<div [ngStyle]="getStyles()">
+  {{objectToShow.desc}}
+</div>
+
+//COMPONENT
+getStyles() {
+    return {'text-decoration': 'underline'};
+}`;
+
   constructor(private highlightService: HighlightService
   ) {
   }
@@ -37,5 +87,9 @@ export class ConditionalStatesComponent implements AfterViewChecked{
 
   conditionalDivClasses() {
     return {'beginner': this.objectToShow.type === 'BEGINNER'}
+  }
+
+  getStyles() {
+    return {'text-decoration': 'underline'};
   }
 }
