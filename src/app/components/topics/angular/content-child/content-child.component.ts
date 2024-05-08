b17468226794172017e0e3fd5527a4c2bd859c40
tomisko <toom134@gmail.com>
2024-05-08 21:39:18 +0200

    topic: angular - content child - code examples

--- src/app/components/topics/angular/content-child/content-child.component.ts
+++ src/app/components/topics/angular/content-child/content-child.component.ts
@@ -11,6 +11,33 @@ export class ContentChildComponent implements AfterViewChecked {
   githubLink: string = "https://github.com/tomisko01/my-demo-platform/blob/main/src/app/components/topics/angular/content-child/content-child.component.ts";
   private highlighted: Boolean = false;
   darkSoulsImageUrl: string = 'assets/img/dark-souls-solaire-praise-the-sun-sticker.jpg';
+  contentChildCode: string =`
+  <!-- HTML -->
+<app-content-child-child>
+  <div #divElement>
+    Text inside div
+  </div>
+</app-content-child-child>
+
+  <!--COMPONENT -->
+@ContentChild('divElement')
+divElement: ElementRef;`;
+
+  contentChildComponentCode: string = `
+  <!-- HTML -->
+<app-content-child-child>
+  <app-content-child-image
+    [imageSrc]="darkSoulsImageUrl">
+  </app-content-child-image>
+</app-content-child-child>
+
+  <!-- HTML CHILD -->
+<ng-content select="app-content-child-image">
+</ng-content>
+
+  <!-- COMPONENT CHILD -->
+@ContentChild(ContentChildImageComponent)
+content: ContentChildImageComponent;`;
 
   constructor(private highlightService: HighlightService
   ) {
