b5635bf9e43c63a90aba8d0fd2b5daa4d6d08411
tomisko <toom134@gmail.com>
2024-05-09 20:27:04 +0200

    topic: angular - content children

--- src/app/components/topics/angular/content-child/component/content-child-child/content-child-child.component.ts
+++ src/app/components/topics/angular/content-child/component/content-child-child/content-child-child.component.ts
@@ -1,4 +1,12 @@
-import {AfterViewInit, Component, ContentChild, ElementRef} from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  ContentChild,
+  ContentChildren,
+  ElementRef,
+  QueryList
+} from '@angular/core';
 import {
   ContentChildImageComponent
 } from "@angularTopic/content-child/component/content-child-image/content-child-image.component";
@@ -8,7 +16,7 @@ import {
   templateUrl: './content-child-child.component.html',
   styleUrl: './content-child-child.component.css'
 })
-export class ContentChildChildComponent implements AfterViewInit {
+export class ContentChildChildComponent implements AfterViewInit, AfterContentInit {
 
   @ContentChild('divElement')
   divElement: ElementRef;
@@ -16,11 +24,19 @@ export class ContentChildChildComponent implements AfterViewInit {
   @ContentChild(ContentChildImageComponent)
   content: ContentChildImageComponent;
 
+  @ContentChildren(ContentChildImageComponent)
+  images: QueryList<ContentChildImageComponent>;
+
   ngAfterViewInit(): void {
     console.log('divElement:');
     console.log(this.divElement);
     console.log('component get by contentChild:');
     console.log(this.content);
+  }
+
+  ngAfterContentInit(): void {
+    console.log('content children:');
+    console.log(this.images);
   }
 
 }
