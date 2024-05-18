cf5208f3267cba835a6f46db296950a961eded06
tomisko <toom134@gmail.com>
2024-05-18 19:52:14 +0200

    topic: angular - styles - host, ng-deep, host-context

--- src/app/components/topics/angular/styles/host/host.component.ts
+++ src/app/components/topics/angular/styles/host/host.component.ts
@@ -11,6 +11,40 @@ export class HostComponent implements AfterViewChecked {
   githubLink: string = "https://github.com/tomisko01/my-demo-platform/blob/main/src/app/components/topics/angular/styles/host/host.component.ts";
   private highlighted: Boolean = false;
 
+  hostCode: string = `
+:host {
+  background-color: #46a35e;
+}`;
+
+  ngDeepCode: string = `
+:host ::ng-deep .deepClass {
+  background-color: #616161;
+}`;
+  themingCode: string = `
+  //HTML
+<div class="aliceblue-theme">
+  <app-host-children>
+  </app-host-children>
+</div>
+
+  //CSS for component
+::ng-deep .aliceblue-theme {
+  color: aliceblue;
+}
+`;
+
+  hostContextCode: string = `
+  //HTML
+<div class="indian-red-border">
+  <app-host-children>
+  </app-host-children>
+</div>
+
+  //CSS for component
+:host-context(.indian-red-border){
+  border: solid 1px indianred;
+}`;
+
   constructor(private highlightService: HighlightService
   ) {
   }
