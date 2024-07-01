44004192cbccdf46cd5838679e80889c7d6a1a47
tomisko <tomnit@proton.me>
2024-07-01 23:12:12 +0200

    topic: angular - defer - on idle

--- src/app/components/topics/angular/defer/defer.component.ts
+++ src/app/components/topics/angular/defer/defer.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {DeferChildComponent} from "@angularTopic/defer/child/defer-child/defer-child.component";
+import {MatDivider} from "@angular/material/divider";
 
 @Component({
   selector: 'app-defer',
   standalone: true,
   imports: [
-    DeferChildComponent
+    DeferChildComponent,
+    MatDivider
   ],
   templateUrl: './defer.component.html',
   styleUrl: './defer.component.css'
