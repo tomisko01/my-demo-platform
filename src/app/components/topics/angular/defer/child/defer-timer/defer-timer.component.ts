b208d49c9c4520072807fe9c5ef667f80ff72ffd
tomisko <tomnit@proton.me>
2024-07-02 22:48:29 +0200

    topic: angular - defer - timer

---
+++ src/app/components/topics/angular/defer/child/defer-timer/defer-timer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-defer-timer',
+  standalone: true,
+  imports: [],
+  templateUrl: './defer-timer.component.html',
+  styleUrl: './defer-timer.component.css'
+})
+export class DeferTimerComponent {
+
+}
--- src/app/components/topics/angular/defer/defer.component.ts
+++ src/app/components/topics/angular/defer/defer.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {DeferChildComponent} from "@angularTopic/defer/child/defer-child/defer-child.component";
 import {MatDivider} from "@angular/material/divider";
+import {DeferTimerComponent} from "@angularTopic/defer/child/defer-timer/defer-timer.component";
 
 @Component({
   selector: 'app-defer',
   standalone: true,
   imports: [
     DeferChildComponent,
-    MatDivider
+    MatDivider,
+    DeferTimerComponent
   ],
   templateUrl: './defer.component.html',
   styleUrl: './defer.component.css'
