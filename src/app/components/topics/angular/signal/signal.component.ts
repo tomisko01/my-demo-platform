db571e0c77ce4c4a007dfc57a52714c55590988f
tomisko <toom134@gmail.com>
2024-07-08 19:15:52 +0200

    topic: angular - signal - don't mutate fields directly

--- src/app/components/topics/angular/signal/signal.component.ts
+++ src/app/components/topics/angular/signal/signal.component.ts
@@ -13,8 +13,21 @@ export class SignalComponent {
 
   counterAsReadonly = this.counter.asReadonly()
 
+  item = signal({
+    id: 1,
+    name: 'Ameno'
+  })
+
   increment() {
     // this.counter.set(this.counter() + 1)
+
+    // DO NOT USE IT IN THAT WAY:
+    // LOSING EVERY BENEFIT FROM SIGNALS
+    // WORKING ONLY FOR DEFAULT CHANGE DETECTION
+    this.item().name = 'Dorime'
+
+    // UPDATE VALUE WITH SET() OR UPDATE() FUNCTIONS
+    this.item.update(data => ({...data, name: 'DORIME'}))
 
     this.counter.update(currentValue => ++currentValue)
   }
