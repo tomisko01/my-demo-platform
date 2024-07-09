d5e4d5137c03703b713e48e1b0efbd4ab0a5b3ac
tomisko <tomnit@proton.me>
2024-07-09 21:08:59 +0200

    topic: angular - signal - computed

--- src/app/components/topics/angular/signal/signal.component.ts
+++ src/app/components/topics/angular/signal/signal.component.ts
@@ -1,9 +1,12 @@
-import {Component, signal} from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
+import {MatDivider} from "@angular/material/divider";
 
 @Component({
   selector: 'app-signal',
   standalone: true,
-  imports: [],
+  imports: [
+    MatDivider
+  ],
   templateUrl: './signal.component.html',
   styleUrl: './signal.component.css'
 })
@@ -12,6 +15,32 @@ export class SignalComponent {
   counter = signal(0)
 
   counterAsReadonly = this.counter.asReadonly()
+
+  // Will always be updated after counter() update
+  derivedCounter = computed(() => {
+      const counter = this.counter()
+      return 10 * counter
+  })
+
+  // Will be updated only when previous compute called counter()
+  derivedCounterWithCondition = computed(() => {
+    if (this.multiplier % 2 == 0) {
+      const counter = this.counter()
+      return 10 * counter
+    }
+    return 0
+  })
+
+  // Will always be updated after counter() update, because we always check counter()
+  fixedDerivedCounterWithCondition = computed(() => {
+    const counter = this.counter()
+    if (this.multiplier % 2 == 0) {
+      return 10 * counter
+    }
+    return 0
+  })
+
+  multiplier = 0
 
   item = signal({
     id: 1,
@@ -30,5 +59,9 @@ export class SignalComponent {
     this.item.update(data => ({...data, name: 'DORIME'}))
 
     this.counter.update(currentValue => ++currentValue)
+  }
+
+  incrementMultiplier() {
+    this.multiplier++
   }
 }
