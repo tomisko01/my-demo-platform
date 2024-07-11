eb4be3c85f346adfeb1dfaa632199823175989e2
tomisko <tomnit@proton.me>
2024-07-11 17:51:50 +0200

    topic: angular - signal - effect with cleanup

--- src/app/components/topics/angular/signal/effect/effect.component.ts
+++ src/app/components/topics/angular/signal/effect/effect.component.ts
@@ -1,11 +1,13 @@
-import {Component, computed, effect, signal} from '@angular/core';
+import {Component, computed, effect, EffectRef, signal} from '@angular/core';
 import {MatDivider} from "@angular/material/divider";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-effect',
   standalone: true,
   imports: [
-    MatDivider
+    MatDivider,
+    MatButton
   ],
   templateUrl: './effect.component.html',
   styleUrl: './effect.component.css'
@@ -19,15 +21,36 @@ export class EffectComponent {
     return 10 * counter
   })
 
+  effectWithCleanUpReference: EffectRef;
+
   constructor() {
+
+    // by default effect will be destroyed together with destroy on component
     effect(() => {
       const counterValue = this.counter()
       const derivedCounterValue = this.derivedCounter()
       console.log('Effect log -', `counterValue: ${counterValue}, derivedCounterValue: ${derivedCounterValue}`)
     })
+
+    this.effectWithCleanUpReference = effect((onCleanup) => {
+
+      onCleanup(() => {
+        console.log(`Cleanup occurred`)
+      })
+
+      const counterValue = this.counter()
+      const derivedCounterValue = this.derivedCounter()
+      console.log(`Effect with cleanUp log -`, `counterValue: ${counterValue}, derivedCounterValue: ${derivedCounterValue}`)
+    }, {
+      manualCleanup: true
+    })
   }
 
-  increment(){
+  increment() {
     this.counter.update(value => value + 1)
+  }
+
+  onCleanup() {
+    this.effectWithCleanUpReference.destroy()
   }
 }
