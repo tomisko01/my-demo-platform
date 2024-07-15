09767844099cc04ab987ae63bec9bf1ea1f89ed2
tomisko <tomnit@proton.me>
2024-07-15 19:58:04 +0200

    how to migrate to standalone components - module adaptation

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,29 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-
+import {AppModule} from './app/app.module';
+/*
+import {bootstrapApplication, BrowserModule} from "@angular/platform-browser";
+import {AppComponent} from "./app/app.component";
+import {importProvidersFrom} from "@angular/core";
+import {provideAnimations} from "@angular/platform-browser/animations";
+import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+*/
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+/*
+REFACTOR TO STANDALONE COMPONENTS
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule),
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom(FormsModule),
+... rest from AppModule
+  ]
+})
+
+ */
