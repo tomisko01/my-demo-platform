654c102f9387a8f99d2b255d818d00e41aeeccdf
tomisko <tomnit@proton.me>
2024-08-10 12:08:13 +0200

    topic: angular - signal - login - use localStorage to preserve user

--- src/app/components/topics/angular/signal/signal-login/service/login-signal.service.ts
+++ src/app/components/topics/angular/signal/signal-login/service/login-signal.service.ts
@@ -1,11 +1,11 @@
-import {computed, inject, Injectable, signal} from '@angular/core';
+import {computed, effect, inject, Injectable, signal} from '@angular/core';
 import {User} from "@angularTopic/signal/signal-login/model/user.model";
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../../../../../environments/environment";
 import {firstValueFrom} from "rxjs";
 
 
-const USER_STORAGE_KEY = 'user';
+const USER_STORAGE_KEY = 'user'
 
 @Injectable({
   providedIn: 'root'
@@ -21,6 +21,23 @@ export class LoginSignalService {
   http = inject(HttpClient)
 
   constructor() {
+    this.loadUserFromStorage()
+
+    effect(() => {
+      const user = this.user()
+      if (user) {
+        localStorage.setItem(USER_STORAGE_KEY, JSON.stringify(user))
+      }
+    })
+  }
+
+  loadUserFromStorage() {
+    const json = localStorage.getItem(USER_STORAGE_KEY)
+
+    if (json) {
+      const user = JSON.parse(json)
+      this.#userSignal.set(user)
+    }
   }
 
   async login(email: string, password: string): Promise<User> {
@@ -34,6 +51,7 @@ export class LoginSignalService {
   }
 
   logout() {
+    localStorage.removeItem(USER_STORAGE_KEY)
     this.#userSignal.set(null)
   }
 }
