015d9d6a17fded7e9aff0ea0abbd0b79587d5d56
tomisko <tomnit@proton.me>
2024-08-20 19:47:43 +0200

    topic: angular - signal - master detail - viewChild as signal

--- src/app/components/topics/angular/signal/signal-master-detail/component/location/location.component.ts
+++ src/app/components/topics/angular/signal/signal-master-detail/component/location/location.component.ts
@@ -1,19 +1,22 @@
-import {Component, inject, signal} from '@angular/core';
+import {Component, ElementRef, inject, OnInit, signal, viewChild} from '@angular/core';
 import {elden} from "../../../../../../../typings";
 import {EldenLocationService} from "@angularTopic/signal/signal-crud/service/elden-location.service";
 import {MatButton} from "@angular/material/button";
+import {MessagesComponent} from "@angularTopic/signal/signal-crud/messages/messages.component";
+import {MatTooltip} from "@angular/material/tooltip";
 
 @Component({
   selector: 'app-location',
   standalone: true,
   imports: [
-    MatButton
+    MatButton,
+    MessagesComponent,
+    MatTooltip
   ],
   templateUrl: './location.component.html',
   styleUrl: './location.component.css'
 })
-export class LocationComponent {
-
+export class LocationComponent implements OnInit {
   mode = signal<'master' | 'detail'>("master")
 
   locations = signal<elden.Location[]>([])
@@ -22,7 +25,24 @@ export class LocationComponent {
 
   locationService = inject(EldenLocationService)
 
+  searchInput = viewChild.required<ElementRef>('search')
+
+  messagesComponent = viewChild<MessagesComponent>(MessagesComponent)
+
+  matTooltipDirective = viewChild(MessagesComponent,
+    {
+      read: MatTooltip
+    })
+
+  ngOnInit(): void {
+    console.log(`messagesComponent as signal viewChild: `, this.messagesComponent())
+    console.log(`matTooltipDirective on messagesComponent as signal viewChild: `, this.matTooltipDirective())
+  }
+
   onSearch() {
 
+    const query = this.searchInput()?.nativeElement.value
+
+    console.log(`search query: `, query)
   }
 }
