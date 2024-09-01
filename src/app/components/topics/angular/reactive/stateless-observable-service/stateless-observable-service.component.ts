6d5ca3de8874cea92bbd42671e4b4f3eea1aea31
tomisko <tomnit@proton.me>
2024-09-01 18:46:55 +0200

    topic: angular - reactivate - stateless observable service - async on view

--- src/app/components/topics/angular/reactive/stateless-observable-service/stateless-observable-service.component.ts
+++ src/app/components/topics/angular/reactive/stateless-observable-service/stateless-observable-service.component.ts
@@ -1,10 +1,38 @@
 import {Component, inject} from '@angular/core';
 import {EldenItemObservableService} from "@angularTopic/reactive/service/elden-item-observable.service";
+import {elden} from "../../../../../typings";
+import {map, Observable} from "rxjs";
+import {ItemListComponent} from "@angularTopic/signal/signal-crud/component/item-list/item-list.component";
+import {LoadingComponent} from "@angularTopic/signal/signal-crud/loading/loading.component";
+import {MatTab, MatTabGroup} from "@angular/material/tabs";
+import {AsyncPipe} from "@angular/common";
+import {
+  MatCard,
+  MatCardContent,
+  MatCardHeader,
+  MatCardImage, MatCardLgImage, MatCardMdImage,
+  MatCardSmImage,
+  MatCardTitle
+} from "@angular/material/card";
 
 @Component({
   selector: 'app-stateless-observable-service',
   standalone: true,
-  imports: [],
+  imports: [
+    ItemListComponent,
+    LoadingComponent,
+    MatTab,
+    MatTabGroup,
+    AsyncPipe,
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardImage,
+    MatCardContent,
+    MatCardSmImage,
+    MatCardMdImage,
+    MatCardLgImage
+  ],
   templateUrl: './stateless-observable-service.component.html',
   styleUrl: './stateless-observable-service.component.css'
 })
@@ -12,9 +40,24 @@ export class StatelessObservableServiceComponent {
 
   eldenItemObservableService = inject(EldenItemObservableService)
 
+  reusableItems$: Observable<elden.Item[]>
+  consumableItems$: Observable<elden.Item[]>
+
+
   constructor() {
-    this.eldenItemObservableService.loadAllItems()
-      .subscribe((items) => console.log(items))
+    const items$ = this.eldenItemObservableService.loadAllItems()
+      .pipe(
+        map(items => items.sort((a, b) => a.name.localeCompare(b.name)))
+      )
+
+    this.reusableItems$ = items$
+      .pipe(
+        map((items) => items.filter(item => item.type === 'Reusable'))
+      )
+    this.consumableItems$ = items$
+      .pipe(
+        map((items) => items.filter(item => item.type === 'Consumable'))
+      )
   }
 
 }
