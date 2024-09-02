2bdba7daf55abc53860efc73fcc402ffaae41ac5
tomisko <tomnit@proton.me>
2024-09-02 20:03:05 +0200

    topic: angular - reactivate - stateless observable service - shareReplay

--- src/app/components/topics/angular/reactive/service/elden-item-observable.service.ts
+++ src/app/components/topics/angular/reactive/service/elden-item-observable.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {map, Observable} from "rxjs";
+import {map, Observable, shareReplay} from "rxjs";
 import {elden} from "../../../../../typings";
 import {environment} from "../../../../../../environments/environment";
 import {GetItemsResponse} from "@angularTopic/signal/signal-crud/service/elden-item-http-client.service";
@@ -17,7 +17,8 @@ export class EldenItemObservableService {
   loadAllItems(): Observable<elden.Item[]> {
     return this.http.get<GetItemsResponse>(`${this.env.apiRoot}/items`)
       .pipe(
-        map((response : GetItemsResponse) => response.items)
+        map((response : GetItemsResponse) => response.items),
+        shareReplay() //to have only one request
       )
   }
 }
