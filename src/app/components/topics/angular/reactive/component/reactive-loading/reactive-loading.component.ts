81738d69b28b071daf7f994547800917e8f7c20f
tomisko <tomnit@proton.me>
2024-09-07 10:05:05 +0200

    topic: angular - reactivate - stateless observable service - loading service define api

--- src/app/components/topics/angular/reactive/component/reactive-loading/reactive-loading.component.ts
+++ src/app/components/topics/angular/reactive/component/reactive-loading/reactive-loading.component.ts
@@ -1,12 +1,15 @@
 import {Component, inject} from '@angular/core';
 import {MatProgressSpinner} from "@angular/material/progress-spinner";
 import {ReactiveLoadingService} from "@angularTopic/reactive/service/reactive-loading.service";
+import {AsyncPipe, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-reactive-loading',
   standalone: true,
   imports: [
-    MatProgressSpinner
+    MatProgressSpinner,
+    AsyncPipe,
+    NgIf
   ],
   templateUrl: './reactive-loading.component.html',
   styleUrl: './reactive-loading.component.css'
--- src/app/components/topics/angular/reactive/service/reactive-loading.service.ts
+++ src/app/components/topics/angular/reactive/service/reactive-loading.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReactiveLoadingService {
 
+  loading$: Observable<boolean>
+
+
   constructor() { }
+
+  showLoaderUntilCompleted<T>(obs$: Observable<T>): Observable<T> {
+    return obs$
+  }
+
+  loadingOn() {
+
+  }
+
+  loadingOff(){
+
+  }
 }
