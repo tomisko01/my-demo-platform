a148f4bd3caa50fec240f9acb00c9e9ed4e54fde
tomisko01 <toom134@gmail.com>
2024-11-03 15:40:17 +0100

    topic: angular-material - drag-and-drop init styles

---
+++ src/app/components/topics/angular-material/drag-and-drop/drag-and-drop.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drag-and-drop',
+  standalone: true,
+  imports: [],
+  templateUrl: './drag-and-drop.component.html',
+  styleUrl: './drag-and-drop.component.css'
+})
+export class DragAndDropComponent {
+
+}
