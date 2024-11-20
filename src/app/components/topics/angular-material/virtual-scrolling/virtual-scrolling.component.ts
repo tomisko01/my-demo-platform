b82fc5423f0e738b1fbbd2372f849413dbe8c538
tomisko01 <toom134@gmail.com>
2024-11-20 18:52:59 +0100

    topic: angular-material - virtual scrolling - use <cdk-virtual-scroll-viewport>

--- src/app/components/topics/angular-material/virtual-scrolling/virtual-scrolling.component.ts
+++ src/app/components/topics/angular-material/virtual-scrolling/virtual-scrolling.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {MatList, MatListItem} from "@angular/material/list";
 import {NgForOf} from "@angular/common";
+import {CdkFixedSizeVirtualScroll, CdkVirtualForOf, CdkVirtualScrollViewport} from "@angular/cdk/scrolling";
 
 @Component({
   selector: 'app-virtual-scrolling',
@@ -8,13 +9,16 @@ import {NgForOf} from "@angular/common";
   imports: [
     MatList,
     MatListItem,
-    NgForOf
+    NgForOf,
+    CdkVirtualScrollViewport,
+    CdkFixedSizeVirtualScroll,
+    CdkVirtualForOf
   ],
   templateUrl: './virtual-scrolling.component.html',
   styleUrl: './virtual-scrolling.component.css'
 })
 export class VirtualScrollingComponent {
 
-  items = Array.from({length:100}).map((item, i) => `Item #${i}`)
+  items = Array.from({length:20000}).map((item, i) => `Item #${i}`)
 
 }
