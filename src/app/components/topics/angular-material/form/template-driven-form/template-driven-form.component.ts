f6862a74227bbbf822b2f748e6d5111d3e8f890f
tomisko01 <toom134@gmail.com>
2024-11-23 19:34:45 +0100

    topic: angular-material - forms - template form field - init

--- src/app/components/topics/angular-material/form/template-driven-form/template-driven-form.component.ts
+++ src/app/components/topics/angular-material/form/template-driven-form/template-driven-form.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import {MatCard, MatCardContent, MatCardTitle} from "@angular/material/card";
+import {MatFormField} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
+import {FormsModule, NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-template-driven-form',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardTitle,
+    MatCardContent,
+    MatFormField,
+    MatInput,
+    MatButton,
+    FormsModule
+  ],
   templateUrl: './template-driven-form.component.html',
   styleUrl: './template-driven-form.component.css'
 })
 export class TemplateDrivenFormComponent {
 
+  onLogin(loginForm: NgForm) {
+    console.log(loginForm.value, loginForm.valid)
+  }
 }
