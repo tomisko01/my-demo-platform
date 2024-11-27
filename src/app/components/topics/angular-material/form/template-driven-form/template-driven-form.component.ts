54c59cc008afd8d403944d61f7887a61e60f4dfd
tomisko01 <toom134@gmail.com>
2024-11-27 20:29:41 +0100

    topic: angular-material - forms - template form field - built in validators

--- src/app/components/topics/angular-material/form/template-driven-form/template-driven-form.component.ts
+++ src/app/components/topics/angular-material/form/template-driven-form/template-driven-form.component.ts
@@ -4,7 +4,7 @@ import {MatError, MatFormField} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
 import {FormsModule, NgForm} from "@angular/forms";
-import {NgIf} from "@angular/common";
+import {JsonPipe, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-template-driven-form',
@@ -18,7 +18,8 @@ import {NgIf} from "@angular/common";
     MatButton,
     FormsModule,
     MatError,
-    NgIf
+    NgIf,
+    JsonPipe
   ],
   templateUrl: './template-driven-form.component.html',
   styleUrl: './template-driven-form.component.css'
