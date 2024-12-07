45f2dc2bf24d172e08a6db8a675060fd4ac13aa1
tomisko01 <toom134@gmail.com>
2024-12-07 12:17:22 +0100

    topic: angular-material - forms - reactive form - form group

--- src/app/components/topics/angular-material/form/reactive-form/reactive-form.component.ts
+++ src/app/components/topics/angular-material/form/reactive-form/reactive-form.component.ts
@@ -3,6 +3,8 @@ import {MatCard, MatCardContent, MatCardTitle} from "@angular/material/card";
 import {MatFormField} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-reactive-form',
@@ -13,11 +15,22 @@ import {MatButton} from "@angular/material/button";
     MatCardContent,
     MatFormField,
     MatInput,
-    MatButton
+    MatButton,
+    ReactiveFormsModule,
+    JsonPipe
   ],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css'
 })
 export class ReactiveFormComponent {
+
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email]
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(8)]
+    }),
+  })
 
 }
