c59f84fb7c95e66f993008bc35cdfa15228d1644
tomisko01 <toom134@gmail.com>
2024-12-09 17:42:38 +0100

    topic: angular-material - forms - reactive form - formBuilder

--- src/app/components/topics/angular-material/form/reactive-form/reactive-form.component.ts
+++ src/app/components/topics/angular-material/form/reactive-form/reactive-form.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {MatCard, MatCardContent, MatCardTitle} from "@angular/material/card";
 import {MatFormField} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {JsonPipe} from "@angular/common";
 import {createPasswordStrengthValidator} from "../../components/password-strength/password-strength.validator";
 
@@ -25,7 +25,9 @@ import {createPasswordStrengthValidator} from "../../components/password-strengt
 })
 export class ReactiveFormComponent {
 
-  email = new FormControl('', {
+  formBuilder = inject(FormBuilder)
+
+  /*email = new FormControl('', {
     validators: [Validators.required, Validators.email],
     updateOn: 'blur'
   })
@@ -39,6 +41,17 @@ export class ReactiveFormComponent {
         createPasswordStrengthValidator()
       ]
     }),
+  })*/
+  form = this.formBuilder.group({
+    email: ['', {
+      validators: [Validators.required, Validators.email],
+      updateOn: 'blur'
+    }],
+    password: ['', [
+      Validators.required,
+      Validators.minLength(8),
+      createPasswordStrengthValidator()
+    ]]
   })
 
 }
