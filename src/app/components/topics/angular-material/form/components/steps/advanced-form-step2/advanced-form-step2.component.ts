27b306b95e0602260b56210a952c3d6cd1acd462
tomisko01 <toom134@gmail.com>
2024-12-20 21:53:30 +0100

    topic: angular-material - forms - disabling controls

--- src/app/components/topics/angular-material/form/components/steps/advanced-form-step2/advanced-form-step2.component.ts
+++ src/app/components/topics/angular-material/form/components/steps/advanced-form-step2/advanced-form-step2.component.ts
@@ -1,6 +1,8 @@
 import {Component, inject} from '@angular/core';
-import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatRadioButton, MatRadioGroup} from "@angular/material/radio";
+import {MatFormField} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
 
 @Component({
   selector: 'app-advanced-form-step2',
@@ -8,7 +10,9 @@ import {MatRadioButton, MatRadioGroup} from "@angular/material/radio";
   imports: [
     ReactiveFormsModule,
     MatRadioGroup,
-    MatRadioButton
+    MatRadioButton,
+    MatFormField,
+    MatInput
   ],
   templateUrl: './advanced-form-step2.component.html',
   styleUrl: './advanced-form-step2.component.css'
@@ -18,6 +22,26 @@ export class AdvancedFormStep2Component {
   fb = inject(FormBuilder)
 
   form = this.fb.group({
-    gameType: ['OFFLINE']
+    gameType: ['OFFLINE'],
+    quantity: [null, [
+      Validators.required,
+      Validators.min(1),
+      Validators.max(999),
+      Validators.pattern("[0-9]+")
+    ]]
   })
+
+  constructor() {
+    this.form.valueChanges.subscribe(
+      val => {
+        const quantityControl = this.form.controls['quantity']
+
+        if (val.gameType === 'OFFLINE' && quantityControl.enabled) {
+          quantityControl.disable({emitEvent: false}) //no trigger valueChange of form
+        } else if (val.gameType === 'ONLINE' && quantityControl.disabled) {
+          quantityControl.enable({emitEvent: false})
+        }
+      }
+    )
+  }
 }
