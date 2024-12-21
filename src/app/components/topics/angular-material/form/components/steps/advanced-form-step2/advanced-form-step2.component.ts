08beb9961ac78597c72f16fbab9b6f1ab97ef5cd
tomisko01 <toom134@gmail.com>
2024-12-21 15:50:57 +0100

    topic: angular-material - forms - dateRange

--- src/app/components/topics/angular-material/form/components/steps/advanced-form-step2/advanced-form-step2.component.ts
+++ src/app/components/topics/angular-material/form/components/steps/advanced-form-step2/advanced-form-step2.component.ts
@@ -1,8 +1,14 @@
 import {Component, inject} from '@angular/core';
 import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatRadioButton, MatRadioGroup} from "@angular/material/radio";
-import {MatFormField} from "@angular/material/form-field";
+import {MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
+import {
+  MatDatepickerModule,
+  MatDatepickerToggle,
+  MatDateRangeInput,
+  MatDateRangePicker
+} from "@angular/material/datepicker";
 
 @Component({
   selector: 'app-advanced-form-step2',
@@ -12,7 +18,13 @@ import {MatInput} from "@angular/material/input";
     MatRadioGroup,
     MatRadioButton,
     MatFormField,
-    MatInput
+    MatInput,
+    MatDateRangeInput,
+    MatLabel,
+    MatDateRangePicker,
+    MatDatepickerToggle,
+    MatDatepickerModule,
+    MatSuffix
   ],
   templateUrl: './advanced-form-step2.component.html',
   styleUrl: './advanced-form-step2.component.css'
@@ -28,7 +40,9 @@ export class AdvancedFormStep2Component {
       Validators.min(1),
       Validators.max(999),
       Validators.pattern("[0-9]+")
-    ]]
+    ]],
+    eventStart: [null],
+    eventEnd: [null]
   })
 
   constructor() {
