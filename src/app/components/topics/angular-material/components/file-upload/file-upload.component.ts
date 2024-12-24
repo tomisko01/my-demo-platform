0994b2dcb224349742f25f931ed6c6bcef027a50
tomisko01 <toom134@gmail.com>
2024-12-24 13:52:32 +0100

    topic: angular-material - forms - validator

--- src/app/components/topics/angular-material/components/file-upload/file-upload.component.ts
+++ src/app/components/topics/angular-material/components/file-upload/file-upload.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, input, signal} from '@angular/core';
+import {Component, forwardRef, inject, input, signal} from '@angular/core';
 import {MatFormField} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatIcon} from "@angular/material/icon";
@@ -8,7 +8,14 @@ import {environment} from "../../../../../../environments/environment";
 import {catchError, finalize, of} from "rxjs";
 import {MatProgressBar} from "@angular/material/progress-bar";
 import {NgIf} from "@angular/common";
-import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  NG_VALIDATORS,
+  NG_VALUE_ACCESSOR,
+  ValidationErrors,
+  Validator
+} from "@angular/forms";
 
 @Component({
   selector: 'app-file-upload',
@@ -24,12 +31,16 @@ import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
   providers: [{
     provide: NG_VALUE_ACCESSOR,
     multi: true,
-    useExisting: FileUploadComponent
+    useExisting: forwardRef(() => FileUploadComponent)
+  }, {
+    provide: NG_VALIDATORS,
+    multi: true,
+    useExisting: forwardRef(() => FileUploadComponent)
   }],
   templateUrl: './file-upload.component.html',
   styleUrl: './file-upload.component.css'
 })
-export class FileUploadComponent implements ControlValueAccessor {
+export class FileUploadComponent implements ControlValueAccessor, Validator {
 
   readonly requiredFileType = input.required<string>()
 
@@ -37,10 +48,13 @@ export class FileUploadComponent implements ControlValueAccessor {
 
   fileName = signal<string>('')
   fileUploadError = signal<boolean>(false)
+  fileUploadSuccess = false
   uploadProgress: number | null = null
   disabled: boolean = false
   onChange = (fileName: string) => {}
   onTouched = () => {}
+  onValidatorChange = () => {
+  }
 
   writeValue(value: any): void {
     this.fileName.set(value)
@@ -58,18 +72,44 @@ export class FileUploadComponent implements ControlValueAccessor {
     this.disabled = isDisabled
   }
 
+  registerOnValidatorChange(fn: () => void): void {
+    this.onValidatorChange = fn
+  }
+
+  validate(control: AbstractControl): ValidationErrors | null {
+    if (this.fileUploadSuccess) {
+      return null
+    }
+
+    if (this.fileName() === '') {
+      return { required: true };
+    }
+
+    let errors: any = {requiredFileType: this.requiredFileType()}
+    if (this.fileUploadError()) {
+      errors.uploadFailed = true
+    }
+
+    return errors
+  }
+
+
   onFileSelected(event: Event) {
     const target = event.target as HTMLInputElement
-    if (target.files) {
+    if (target.files && target.files.length > 0) {
       const file: File = target.files[0]
-      if (file) {
-        this.fileName.set(file.name)
-        this.fileUploadError.set(false)
-        this.uploadProgress = 0.1
-        this.uploadFile(file)
-      } else {
-        this.fileName.set('')
-      }
+
+      this.fileName.set(file.name)
+      this.fileUploadError.set(false)
+      this.uploadProgress = 0.1
+      this.uploadFile(file)
+    } else {
+      this.fileName.set('')
+      this.fileUploadError.set(false)
+      this.fileUploadSuccess = false
+      this.onChange('')
+
+      this.onValidatorChange()
     }
   }
 
@@ -90,7 +130,9 @@ export class FileUploadComponent implements ControlValueAccessor {
       if (event.type == HttpEventType.UploadProgress) {
         this.uploadProgress = Math.round(100 * (event.loaded / event.total))
       } else if (event.type == HttpEventType.Response) {
+        this.fileUploadSuccess = true
         this.onChange(this.fileName())
+        this.onValidatorChange()
       }
     })
   }
@@ -99,4 +141,5 @@ export class FileUploadComponent implements ControlValueAccessor {
     this.onTouched()
     fileUpload.click()
   }
+
 }
