186e825ac97f5d5fd90dc4d162c0eb9428fc0d51
tomisko01 <toom134@gmail.com>
2024-12-27 20:33:18 +0100

    topic: angular-material - forms - step3 - init

--- src/app/components/topics/angular-material/form/components/steps/advanced-form-step3/advanced-form-step3.component.ts
+++ src/app/components/topics/angular-material/form/components/steps/advanced-form-step3/advanced-form-step3.component.ts
@@ -1,10 +1,24 @@
 import {Component, inject} from '@angular/core';
-import {FormBuilder} from "@angular/forms";
+import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
+import {MatMiniFabButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatOption, MatSelect} from "@angular/material/select";
 
 @Component({
   selector: 'app-advanced-form-step3',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatMiniFabButton,
+    MatIcon,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatSelect,
+    MatOption
+  ],
   templateUrl: './advanced-form-step3.component.html',
   styleUrl: './advanced-form-step3.component.css'
 })
