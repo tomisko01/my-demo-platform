b1bcc07b148faf8fd1baf5aed250dc1c1b290932
tomisko01 <toom134@gmail.com>
2024-12-28 12:23:46 +0100

    topic: angular-material - forms - step3 - formArray

--- src/app/components/topics/angular-material/form/components/steps/advanced-form-step3/advanced-form-step3.component.ts
+++ src/app/components/topics/angular-material/form/components/steps/advanced-form-step3/advanced-form-step3.component.ts
@@ -1,10 +1,11 @@
 import {Component, inject} from '@angular/core';
-import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
+import {FormArray, FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatMiniFabButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatOption, MatSelect} from "@angular/material/select";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-advanced-form-step3',
@@ -17,7 +18,8 @@ import {MatOption, MatSelect} from "@angular/material/select";
     MatInput,
     MatLabel,
     MatSelect,
-    MatOption
+    MatOption,
+    NgForOf
   ],
   templateUrl: './advanced-form-step3.component.html',
   styleUrl: './advanced-form-step3.component.css'
@@ -26,5 +28,24 @@ export class AdvancedFormStep3Component {
 
   fb = inject(FormBuilder)
 
-  form = this.fb.group({})
+  form = this.fb.group({
+    locations: this.fb.array([])
+  })
+
+  get locations(): FormArray {
+    return this.form.controls['locations'] as FormArray
   }
+
+  addLocation() {
+    const newLocationForm = this.fb.group({
+      name: ['', Validators.required],
+      dlc: ['BASE_GAME', Validators.required]
+    })
+
+    this.locations.push(newLocationForm)
+  }
+
+  deleteLocation(locationIndex: number) {
+    this.locations.removeAt(locationIndex)
+  }
+}
